db7810c800afd2e530e1f2d61541ddbf3e761396
Smartbizapk <smartbizyt335@gmail.com>
2025-06-14 14:57:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,3 +9,23 @@ const App = () => (
 );
 
 export default App;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Welcome from "./modules/Welcome";
+import Module1 from "./modules/Module1";
+import Module2 from "./modules/Module2";
+// Baaki modules bhi yahi import karo
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/module1" element={<Module1 />} />
+        <Route path="/module2" element={<Module2 />} />
+        {/* Sabhi 12 modules ka route yahi add karo */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
